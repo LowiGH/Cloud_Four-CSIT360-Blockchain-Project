05ad97ab000fe4b7d9ab2f9d9a3608b92f32b1e2
Matthew Rimar Martus <matthewrimar1102@gmail.com>
2025-09-19 12:48:37 +0800

    feat (backend) Added the springboot file

---
+++ backend/Cloud.Four/src/test/java/edu/cit/csit360/Cloud/Four/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.csit360.Cloud.Four;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
